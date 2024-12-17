56065e8af09a0baf5e91362160bc5eec3753cc51
Caleb Meinke <cameinke@gmail.com>
2024-12-16 20:40:01 -0600

    Week 9 HW upload

---
+++ EmployeeSQL/employee_schema.sql
@@ -0,0 +1,52 @@
+-- Create "departments" table
+CREATE TABLE departments (
+    dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (dept_no)
+);
+
+-- Create "titles" table
+CREATE TABLE titles (
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (title_id)
+);
+
+-- Create "employees" table
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (emp_no),
+    CONSTRAINT fk_employees_emp_title_id FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+-- Create "dept_emp" table
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no, dept_no),
+    CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+-- Create "dept_manager" table
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (dept_no, emp_no),
+    CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+-- Create "salaries" table
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (emp_no),
+    CONSTRAINT fk_salaries_emp_no FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
